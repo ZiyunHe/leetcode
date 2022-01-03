981e93f1005b94830fd343a6834a592c0ea4c823
Ziyun <ziyunhe@gmail.com>
2022-01-03 05:05:29 -0800

    change best solution

--- 34_FindFirstLastPositionofElementinSortedArray/best_solution.py
+++ 34_FindFirstLastPositionofElementinSortedArray/best_solution.py
@@ -1,39 +1,29 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums) - 1
         
-        def binarySearch(nums,start,end):
-            if start>end:
-                return -1
-            
-            mid = start + (end-start)//2
-            
-            if nums[mid] == target:
-                return mid
-            elif nums[mid]>target:
-                return binarySearch(nums,start,mid-1)
+        found = False
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
-                return binarySearch(nums,mid+1,end)
-            
-            
-        indx = binarySearch(nums,0,len(nums)-1)
-        
-        if indx == -1:
-            return [-1,-1]
-        left_most = indx
-        right_most = indx
-        
-        while True:
-            indx = binarySearch(nums,0,indx-1)
-            
-            if indx == -1:
+                found = True
                 break
-                
-            left_most = indx
-        while True:
-            indx = binarySearch(nums,right_most+1,len(nums)-1)
+        
+        if not found:
+            return [-1, -1]
             
-            if indx == -1:
+        ans = [0, 0]
+        for i in range(left, mid + 1):
+            if nums[i] == target:
+                ans[0] = i
                 break
-            
-            right_most = indx
-        return [left_most,right_most]
+        for i in range(right, mid - 1, -1):
+            if nums[i] == target:
+                ans[1] = i
+                break
+        return ans
