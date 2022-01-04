af0d0d7dbe709262c9f64b7d46d4860f62fcdca7
Ziyun <ziyunhe@gmail.com>
2022-01-04 04:09:28 -0800

    33

---
+++ 33_SearchInRotatedSortedArray/1_4_21.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        s, e = 0, len(nums) - 1
+        while s <= e:
+            m = (s + e) // 2
+            if m + 1 < len(nums) and nums[m + 1] < nums[m]:
+                s = m + 1
+            elif m - 1 > -1 and nums[m - 1] < nums[m]:
+                e = m - 1
+        left = self.bs(nums[0:s], target)
+        right = self.bs(nums[s:], target)
+        if not left == -1:
+            return left
+        if not right == -1:
+            return len(nums[0:s]) + right
+        return -1
+    
+    def bs(self, nums:List[int], target:int) -> int:
+        if not nums:
+            return -1
+        s, e = 0, len(nums) - 1
+        while s <= e:
+            m = (s + e) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                s = m + 1
+            else:
+                e = m - 1
+        return -1
