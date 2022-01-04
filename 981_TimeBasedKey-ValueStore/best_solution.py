a8946032b768d9b55e0f828a8a6c798d46ad8968
Ziyun <ziyunhe@gmail.com>
2022-01-04 04:42:48 -0800

    981

--- 981_TimeBasedKey-ValueStore/best_solution.py
+++ 981_TimeBasedKey-ValueStore/best_solution.py
@@ -1,36 +1,21 @@
-from collections import defaultdict
-
 class TimeMap:
 
     def __init__(self):
-        self._timestamped_key_values = defaultdict(list)
+        self.timestamps = collections.defaultdict(list)
+        self.values = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        timestamped_values = self._timestamped_key_values[key]
+        self.timestamps[key].append(timestamp)
+        self.values[key].append(value)
         
-        if not timestamped_values or timestamped_values[-1][1] != value:
-            timestamped_values.append((timestamp, value))    
 
     def get(self, key: str, timestamp: int) -> str:
-        timestamped_values = self._timestamped_key_values[key]
-        
-        if not timestamped_values or timestamped_values[0][0] > timestamp:
-            return ""
-        
-        lo, hi = 0, len(timestamped_values) - 1
-        
-        while lo < hi:
-            mid = (hi + lo + 1) // 2
-            mid_timestamp = timestamped_values[mid][0]
-            
-            if mid_timestamp < timestamp:
-                lo = mid
-            elif mid_timestamp == timestamp:
-                lo = hi = mid
-            else:
-                hi = mid - 1
-        
-        return timestamped_values[lo][1]
+        if key not in self.values:
+            return ''
+        idx = bisect.bisect_right(self.timestamps[key], timestamp)
+        if idx == 0:
+            return ''
+        return self.values[key][idx-1]
 
 
 # Your TimeMap object will be instantiated and called as such:
