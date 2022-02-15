04f5e2d1217b41631ad76e3fe8ec3df75b52d409
ziyun <ziyunhe@gmail.com>
2022-02-15 08:37:26 -0800

    668

--- 668_kthSmallestNumInMultiplicationTable/best_solution.py
+++ 668_kthSmallestNumInMultiplicationTable/best_solution.py
@@ -14,3 +14,22 @@ class Solution(object):
             else:
                 hi = mi
         return lo
+
+
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        def less_than(mid:int) -> int:
+            count = 0
+            for i in range(1, m+1):
+                count += min(mid // i, n)
+            return count
+
+        s, e = 0, m*n - 1
+        while s<=e:
+            mid = (s+e) // 2
+            if less_than(mid) < k:
+                s = mid + 1
+            else:
+                e = mid - 1
+        return s
+        
