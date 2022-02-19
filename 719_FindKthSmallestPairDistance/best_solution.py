205426d6b946d8e8a835a0f72dae1b67b28b2523
ziyun <ziyunhe@gmail.com>
2022-02-18 22:54:33 -0800

    bs template

--- 719_FindKthSmallestPairDistance/best_solution.py
+++ 719_FindKthSmallestPairDistance/best_solution.py
@@ -19,3 +19,46 @@ class Solution:
             else:
                 right = mid
         return left
+
+
+def pairs(self, nums, x):
+        pairs, i = 0, 0
+        for j in range(len(nums)):
+            while nums[j] - nums[i] > x:
+                i = i+1
+            pairs = pairs + (j-i)
+        return pairs
+...
+if pairs < k: s = m+1
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return -1
+        
+        nums.sort()
+        start, end = 0, nums[-1] - nums[0]
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if self.is_possible(nums, mid) < k:
+                start = mid
+            else:
+                end = mid
+        
+        if self.is_possible(nums, start) >= k:
+            return start
+        
+        if self.is_possible(nums, end) >= k:
+            return end
+        
+        return -1       
+        
+    def is_possible(self, nums, diff):
+        i = res = 0
+        for j in range(1, len(nums)):
+            while nums[j] - nums[i] > diff:
+                i += 1
+            
+            res += j - i
+            
+        return res # num of pair <= diff 
+            
